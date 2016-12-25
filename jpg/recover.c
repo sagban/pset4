3f3dd83499ccf5c6948eaed7b5bffbff5fe65093
Sagar Bansal <sagarbansal099@gmail.com>
2016-12-25 19:00:29 +0000

    another way

--- jpg/recover.c
+++ jpg/recover.c
@@ -12,51 +12,49 @@
 int main(void)
 {
 		//initilized filename
-		char fname[50];
+		char fname[500];
 		
 		//iniltzed a buffer
 		unsigned char buffer[512];
 		
 		int count=0;
 		char* infile = "card.raw";
-		FILE* outptr;
+		FILE* outptr = NULL;
 		
 		//open card.raw
 		FILE* inptr = fopen(infile,"r");
+		
+		//read from card.raw to buffer
+		
 		// check for end of file
-		while(fread(&buffer, sizeof(int)*128, 1, inptr) == 1)
+		while(fread(buffer, 512, 1, inptr) == 1)
 		{
-			    //read from card.raw to buffer
-				fread(&buffer, sizeof(int)*128, 1, inptr);
-				
+
 				//check for begining of JPEG
 				if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe0 || buffer[3] == 0xe1))
 				{
 					//print the jpg into same directory
 					sprintf(fname, "%03d.jpg", count);
+					//open the jpg file
+					
+					outptr = fopen(fname,"w");
+					fclose(outptr);
 					outptr = fopen(fname,"w");
 					count++;
-					while(1)
-					{
-						fwrite(&buffer, sizeof(int)*128, 1, outptr);
-						fread(&buffer, sizeof(int)*128, 1, inptr);
-						if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe0 || buffer[3] == 0xe1))
-						{
-							fseek(inptr,-512,SEEK_CUR);
-							fclose(outptr);
-							break;
-						}
-					}
+				}
+				
+				if(outptr !=NULL)
+				{
+					fwrite(buffer,512,1,outptr);
 
 				}
 
-	
 		}
 		
 		// close infile
 		fclose(inptr);
 		
-		// Thats all folk!
+		// Thats, all folk!
 		return 0;
 	
 }
